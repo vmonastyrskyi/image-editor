a3718229b5b87724b0df34a05368319b068fce39
Vladyslav Monastyrskyi <Vladyslav_Monastyrskyi@epam.com>
2019-04-25 14:53:16 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+              
+            </div>
+        );
+    }
+}
+
+export default App;
