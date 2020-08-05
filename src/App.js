7f06ea7b5799baeee57c21e1582c5f8c997b7d55
monastyrskyi <vladyslav.monastyrskyi@gmail.com>
2020-08-05 15:09:35 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-    constructor() {
-        super()
-    }
-
-    render() {
-        return (
-            <div>
-              
-            </div>
-        );
-    }
+  render() {
+    return <></>;
+  }
 }
 
 export default App;
